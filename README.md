a1d3f788f0ec340a1efb6baa37e0d96d5e9d48f5
akashpk41 <akashpk84100@gmail.com>
2022-04-18 16:35:22 +0600

    added about me component and all code is complete

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 
 ## Fetures of this website.
 
-* This is a Dental Doctors personal website, where you can view doctor services package and choose one package suit on you. and book that package .
+* This is a Dental Doctors personal website, where you can view doctor services package and choose one package  for you and book that packages .
 
 * If you want to book a package you should want to login or register first. then go to checkout page fill up your personal information and  submit .
 * Many patients review are also there.
 
 ## Main Fetures.
-* You can create your account by your `email` and `password` and login.
-* If you forgot your password you can reset. 
+* You can create your account with your email and password and after create your account you can login
+* If you forgot your password you can reset.
 * Also you can Login with your `Google` or `Facebook` account.
 
 ## Tecnologies used in this website
 
 * This is fully responsive website .
-* I used `Tailwind css` `React` and `Firebase`.
+* I used `Tailwind css` `React` and `Firebase 9 `.
--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-
+import React from "react";
+import akashPk from "../../../images/akash-pk.jpg";
 const About = () => {
-     return (
-          <div className='h-screen'>
-               this is about page
-          </div>
-     );
+  return (
+    <div className="my-10">
+      <div className="max-w-sm p-5 md:mx-auto hover:scale-105 cursor-pointer duration-200  mx-2 bg-white rounded-lg border border-gray-200 shadow-2xl   ">
+        <img className=" w-96 h-96 rounded-full" src={akashPk} alt="" />
+
+        <div className="p-5 text-center ">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+            Akash PK
+          </h5>
+
+          <p className="mb-3 font-normal  leading-8 text-gray-700 ">
+            Although height is 5 feet 8 inches, But my dream is sky-high🔥{" "}
+            <br />
+            Already i started working on my dream . <br />
+            Really my dream is so big. I want to make my own company like Google
+            or Amazon . Its funny but i believe on myself, <br />{" "}
+            <span className=" italic  ">
+              I can and I will make it Inshallah
+            </span>{" "}
+            💥
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
